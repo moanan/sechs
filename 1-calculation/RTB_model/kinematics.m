ac1a6b88362da76331584ced5288f735907e5142
An Mo <mo@is.mpg.de>
2020-01-30 13:50:16 +0800

    update DH model

--- 1-calculation/RTB_model/kinematics.m
+++ 1-calculation/RTB_model/kinematics.m
@@ -5,7 +5,7 @@ L(2) = Link('revolute', 'd', 0.06,    'a', 0.2,   'alpha', 0);
 L(3) = Link('revolute', 'd', 0, 'a', 0,   'alpha', pi/2);
 L(4) = Link('revolute', 'd', 0.2,    'a', 0,   'alpha', -pi/2);
 L(5) = Link('revolute', 'd', 0.1,    'a', 0,     'alpha', pi/2);
-L(6) = Link('revolute', 'd', 0.03,    'a', 0,     'alpha', 0);
+L(6) = Link('revolute', 'd', 0.08,    'a', 0,     'alpha', 0);
 
 sechs = SerialLink(L, 'name', 'sechs')
 
