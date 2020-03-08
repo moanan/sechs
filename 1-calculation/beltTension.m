a943b4572140d33b7e8393abc4167a5b743514c0
An Mo <mo@is.mpg.de>
2020-03-08 17:20:41 +0100

    update timing belt calculation

--- 1-calculation/beltTension.m
+++ 1-calculation/beltTension.m
@@ -6,7 +6,8 @@
 % F - bele tenstion [N]
 
 % An Mo
-% 2019-08-18
+% update 2020-03-08
+% create 2019-08-18
 
 clc;clear;close all;
 
@@ -17,14 +18,16 @@ for j = 1:length(T)
     for i = 1:length(r)
         F(i) = T(j)/r(i) * 1000;
     end
-    plot(r, F, 'lineWidth', 2, 'DisplayName', ['Torque: ',num2str(T(j)), ' [Nm]']); hold on;
+    plot(r*2, F, 'lineWidth', 2, 'DisplayName', ['Torque: ',num2str(T(j)), ' [Nm]']); hold on;
 end
-title('Belt tension estimation');
-xlabel('Output pulley radius [mm]'); ylabel('Timing belt tension [N]');
+title('Belt tension estimation (5mm pitch)');
+xlabel('Output pulley diameter [mm]'); ylabel('Timing belt tension [N]');
 grid on; grid minor; legend show;
 
-plot([r(1) r(end)], [300 300], '--', 'lineWidth', 1, 'DisplayName', '6mm belt: 300 [N]');
-plot([r(1) r(end)], [599 599], '--', 'lineWidth', 1, 'DisplayName', '10mm belt: 599 [N]');
+plot([r(1)*2 r(end)*2], [350 350], '--', 'lineWidth', 1, 'DisplayName', 'AT5 6mm: 350 [N]');
+plot([r(1)*2 r(end)*2], [700 700], '--', 'lineWidth', 1, 'DisplayName', 'AT5 10mm: 700 [N]');
+plot([r(1)*2 r(end)*2], [1260 1260 ], '--', 'lineWidth', 1, 'DisplayName', 'AT5 16mm: 1260 [N]');
 
 
 print('-painters','-depsc', 'belt_tension');
+print('-painters','-dpng', 'belt_tension');
