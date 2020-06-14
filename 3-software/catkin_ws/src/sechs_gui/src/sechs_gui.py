2d7d3740cde9936d48a3d75134fa236cd10dbdc5
Yue <dbddqy@gmail.com>
2020-06-14 06:56:49 +0200

    gui A2 finish

---
+++ 3-software/catkin_ws/src/sechs_gui/src/sechs_gui.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+import rospy
+import time
+
+from sechs_odrive.msg import Sechs_Axes
+
+
+def value_adjust():
+    global labels_value, VALUS
+    if control_mode_var.get() == 0:
+        for i in range(6):
+            labels_value[i].config(text=("%.3f" % VALUS[i]))
+    else:
+        # **************************
+        # to be developed
+        # **************************
+        pass
+
+
+def button_plus_clicked(index):
+    global PUB, VALUS_SET
+    VALUS_SET.values[index] += 0.005
+    PUB.publish(VALUS_SET)
+
+
+def button_minus_clicked(index):
+    global PUB, VALUS_SET
+    VALUS_SET.values[index] -= 0.005
+    PUB.publish(VALUS_SET)
+
+
+def function_config():
+    global labels_control
+     # Axes
+    if control_mode_var.get() == 0:
+        for i in range(6):
+            labels_control[i].config(text=("A%d"%(i+1)))
+    else:
+        labels_control[0].config(text="X")
+        labels_control[1].config(text="Y")
+        labels_control[2].config(text="Z")
+        labels_control[3].config(text="A")
+        labels_control[4].config(text="B")
+        labels_control[5].config(text="C")
+
+
+window = tk.Tk()
+window.title("sechs")
+window.geometry("400x300")
+
+# ===============
+# function_select
+# ===============
+
+function_select_f = tk.Frame(window)
+function_select_f.pack()
+
+control_mode_var = tk.IntVar()
+function1_rb = tk.Radiobutton(function_select_f, text='Axes', font=12, variable=control_mode_var, value=0, command=function_config)
+function1_rb.pack(side="left")
+function2_rb = tk.Radiobutton(function_select_f, text='Cartesian Base', font=12, variable=control_mode_var, value=1, command=function_config)
+function2_rb.pack(side="left")
+function3_rb = tk.Radiobutton(function_select_f, text='Cartesian TCF', font=12, variable=control_mode_var, value=2, command=function_config)
+function3_rb.pack(side="left")
+
+# ================
+# 6 inputs outputs
+# ================
+
+control_panel_f = tk.Frame(window)
+control_panel_f.pack()
+
+
+def add_control_panel(index):
+    control_panel_temp = tk.Frame(control_panel_f)
+    control_panel_temp.pack()
+
+    label_control = tk.Label(control_panel_temp, text=("A%d" % (index+1)), width=8, height=1, bg="white", font=12)
+    label_control.pack(side="left", padx=10)
+    label_value = tk.Label(control_panel_temp, text="0.0", width=8, height=1, bg="white", font=12)
+    label_value.pack(side="left", padx=10)
+
+    tk.Button(control_panel_temp, text="+", command=lambda: button_plus_clicked(index)).pack(side="left")
+    tk.Button(control_panel_temp, text="-", command=lambda: button_minus_clicked(index)).pack(side="left")
+
+    return label_control, label_value
+
+
+labels_control, labels_value = [], []
+for i in range(6):
+    lc, lv = add_control_panel(i)
+    labels_control.append(lc)
+    labels_value.append(lv)
+
+# =================
+# ROS communication
+# =================
+
+VALUS = [0.0] * 6
+VALUS_SET = Sechs_Axes()
+
+
+def callback_encoder_feedback(sechs_axes):
+    for i in range(6):
+        VALUS[i] = sechs_axes.values[i]
+    value_adjust()
+
+flag_continue = False
+rospy.init_node('gui', anonymous=False)
+# time.sleep(5)
+rospy.Subscriber('Sechs/Encoder_Feedback', Sechs_Axes, callback_encoder_feedback)
+PUB = rospy.Publisher('Sechs/Position_Command', Sechs_Axes , queue_size=10)
+
+window.mainloop()
+
+rospy.spin()
--- 3-software/catkin_ws/src/sechs_odrive/src/odrive_interface.py
+++ 3-software/catkin_ws/src/sechs_odrive/src/odrive_interface.py
@@ -5,10 +5,10 @@ from __future__ import print_function
 import odrive
 import time
 from odrive.enums import *
-# from math import pi
+from math import pi
 
 import rospy
-from sechs_odrive.msg import Position_Command
+from sechs_odrive.msg import Sechs_Axes
 # from sensor_msgs.msg import Joy
 
 
@@ -19,13 +19,13 @@ class Sechs:
         # find odrives and load axes
         # --------------------------
 
-        odrv0 = odrv0 = odrive.find_any(serial_number="208A3592524B") # 56V
+        odrv0 = odrive.find_any(serial_number="208A3592524B") # 56V
         # odrv1 = odrive.find_any(serial_number="2083359F524B") # 24V
         # odrv2 = None
         self.axes = []
         self.axes.append(odrv0.axis0)
         self.axes.append(odrv0.axis1)
-        # axes.append(odrv1.axis0)
+        # self.axes.append(odrv1.axis0)
         # axes.append(odrv1.axis1)
         # axes.append(odrv2.axis0)
         # axes.append(odrv2.axis1)
@@ -54,31 +54,56 @@ class Sechs:
         #     self.axes[i].config.startup_encoder_index_search = True
         #     # ...
 
-        i = 1
-        self.axes[i].config.startup_encoder_index_search = rospy.get_param(("/axis_%d/startup_encoder_index_search" % i))
-        self.axes[i].motor.config.pre_calibrated = rospy.get_param(("/axis_%d/motor_config/pre_calibrated" % i))
-        self.axes[i].motor.config.pole_pairs = rospy.get_param(("/axis_%d/motor_config/pole_pairs" % i))
-        self.axes[i].motor.config.current_lim = rospy.get_param(("/axis_%d/motor_config/current_lim" % i))
-        self.axes[i].motor.config.calibration_current = rospy.get_param(("/axis_%d/motor_config/calibration_current" % i))
-        self.axes[i].motor.config.current_lim_tolerance = rospy.get_param(("/axis_%d/motor_config/current_lim_tolerance" % i))
-        self.axes[i].encoder.config.pre_calibrated = rospy.get_param(("/axis_%d/encoder_config/pre_calibrated" % i))
-        self.axes[i].encoder.config.cpr = rospy.get_param(("/axis_%d/encoder_config/cpr" % i))
-        self.axes[i].encoder.config.use_index = rospy.get_param(("/axis_%d/encoder_config/use_index" % i))
-        self.axes[i].controller.config.vel_limit = rospy.get_param(("/axis_%d/controller_config/vel_limit" % i))
-        self.axes[i].controller.config.vel_limit_tolerance = rospy.get_param(("/axis_%d/controller_config/vel_limit_tolerance" % i))
-        self.axes[i].controller.config.pos_gain = rospy.get_param(("/axis_%d/controller_config/pos_gain" % i))
-        self.axes[i].controller.config.vel_gain = rospy.get_param(("/axis_%d/controller_config/vel_gain" % i))
-        self.axes[i].controller.config.vel_integrator_gain = rospy.get_param(("/axis_%d/controller_config/vel_integrator_gain" % i))
-        self.axes[i].controller.config.control_mode = rospy.get_param(("/axis_%d/controller_config/control_mode" % i))
+        for i in range(1, 2):
+            self.axes[i].config.startup_encoder_index_search = rospy.get_param(("/axis_%d/startup_encoder_index_search" % i))
+            self.axes[i].motor.config.pre_calibrated = rospy.get_param(("/axis_%d/motor_config/pre_calibrated" % i))
+            self.axes[i].motor.config.pole_pairs = rospy.get_param(("/axis_%d/motor_config/pole_pairs" % i))
+            self.axes[i].motor.config.current_lim = rospy.get_param(("/axis_%d/motor_config/current_lim" % i))
+            self.axes[i].motor.config.calibration_current = rospy.get_param(("/axis_%d/motor_config/calibration_current" % i))
+            self.axes[i].motor.config.current_lim_tolerance = rospy.get_param(("/axis_%d/motor_config/current_lim_tolerance" % i))
+            self.axes[i].encoder.config.pre_calibrated = rospy.get_param(("/axis_%d/encoder_config/pre_calibrated" % i))
+            self.axes[i].encoder.config.cpr = rospy.get_param(("/axis_%d/encoder_config/cpr" % i))
+            self.axes[i].encoder.config.use_index = rospy.get_param(("/axis_%d/encoder_config/use_index" % i))
+            self.axes[i].controller.config.vel_limit = rospy.get_param(("/axis_%d/controller_config/vel_limit" % i))
+            self.axes[i].controller.config.vel_limit_tolerance = rospy.get_param(("/axis_%d/controller_config/vel_limit_tolerance" % i))
+            self.axes[i].controller.config.pos_gain = rospy.get_param(("/axis_%d/controller_config/pos_gain" % i))
+            self.axes[i].controller.config.vel_gain = rospy.get_param(("/axis_%d/controller_config/vel_gain" % i))
+            self.axes[i].controller.config.vel_integrator_gain = rospy.get_param(("/axis_%d/controller_config/vel_integrator_gain" % i))
+            self.axes[i].controller.config.control_mode = rospy.get_param(("/axis_%d/controller_config/control_mode" % i))
+    
+    def start_closed_loop(self):
+        for i in range(1, 2):
+            self.axes[i].requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL 
+
+    @property
+    def axes_value(self):
+        sensor_msg = Sechs_Axes()
+        for i in range(1, 2):
+            sensor_msg.values[i] = self.axes[i].encoder.pos_estimate * pi / 2000.0 / self.reductions[i]
+        return sensor_msg
+    
+    def move_axes(self, values):
+        for i in range(1, 2):
+            sechs.axes[i].controller.move_to_pos(values[i] * 2000.0 * sechs.reductions[i] / pi)
 
 
-def callback_pos_command(pos_command):
-    rospy.loginfo("pos_1: %f", pos_command.pos[1])
+def callback_pos_command(sechs_axes):
+    global sechs
+    rospy.loginfo("pos_1: %f", sechs_axes.values[1])
+    sechs.move_axes(sechs_axes.values)
 
 
 rospy.init_node('odrive_interface', anonymous=False)
 sechs = Sechs()
+sechs.start_closed_loop()
 
 # rospy.loginfo("%f", sechs.axes[1].controller.config.vel_gain)
-rospy.Subscriber('Sechs/Position_Command', Position_Command, callback_pos_command)
+rospy.Subscriber('Sechs/Position_Command', Sechs_Axes, callback_pos_command)
+pub = rospy.Publisher('Sechs/Encoder_Feedback', Sechs_Axes , queue_size=10)
+
+rate = rospy.Rate(100) 
+while not rospy.is_shutdown():
+    pub.publish(sechs.axes_value)
+    rate.sleep()
+
 rospy.spin()
